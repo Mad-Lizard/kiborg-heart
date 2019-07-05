515e057ed92127e860dfef878feb28b8802fbf50
annamavka <annamavka@gmail.com>
2019-07-05 14:04:26 +0300

    Added views for post_list and post_detail with CSS

--- recovery/views.py
+++ recovery/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+#import datetime
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
+    return render(request, 'recovery/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'recovery/post_detail.html', {'post':post})
